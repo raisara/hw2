46a768b888830f4f206accfb6b74f4ba92251a66
raisara <49322828+raisara@users.noreply.github.com>
2021-08-16 12:42:46 +0400

    Update P36.kt

--- hw2/src/arithmetic/P36.kt
+++ hw2/src/arithmetic/P36.kt
@@ -1,8 +1,31 @@
 package arithmetic
 //uncompleted
+/*fun Int.primeFactorMultiplicity(): List<Pair<Int, Int>>{
+    var count=1
+    val primeFactors = (this.primeFactors()).toMutableList()//?
+    val primeFactorsWithPair = ArrayList<Pair<Int, Int>>()
+    var i =0;
+    while(i<primeFactors.size-1){
+        if(i!=primeFactors.size-1){
+            if(primeFactors[i] == primeFactors[i+1]){
+                count++
+            }else{
+                primeFactorsWithPair.add(primeFactors[i] to count)
+                count = 1
+            }
+            i++
+        } else{
+            primeFactorsWithPair.add(primeFactors[i] to 1)
+        }
+    }
+    return primeFactorsWithPair
+}
+*/
+
 fun Int.primeFactorMultiplicity(): List<Pair<Int, Int>>{
     var count=1
     val primeFactors = (this.primeFactors()).toMutableList()//?
+    var that = primeFactors[0]
     val primeFactorsWithPair = ArrayList<Pair<Int, Int>>()
     var i =0;
     while(i<primeFactors.size-1){
@@ -10,11 +33,12 @@ fun Int.primeFactorMultiplicity(): List<Pair<Int, Int>>{
             count++
         }else{
             primeFactorsWithPair.add(primeFactors[i] to count)
+            that=primeFactors[i+1]
             count = 1
         }
         i++
     }
-
+    primeFactorsWithPair.add(that to 1)
     return primeFactorsWithPair
 }
 
