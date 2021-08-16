8963d12e599618f2bab573eed8c7c76601fc17d6
raisara <49322828+raisara@users.noreply.github.com>
2021-08-16 08:57:51 +0400

    Update comparison.kt

--- hw2/src/conventions/comparison.kt
+++ hw2/src/conventions/comparison.kt
@@ -1,2 +1,18 @@
 package conventions
 
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(date2: MyDate): Int {
+        if(year!=date2.year){
+            return year-date2.year
+        }else if(month!=date2.month){
+            return month-date2.month
+        }else{
+            return dayOfMonth-date2.dayOfMonth
+        }
+    }
+}
+
+fun test(date1: MyDate, date2: MyDate) {
+    // this code should compile:
+    println(date1 < date2)
+}
